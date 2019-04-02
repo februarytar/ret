39e3bcb6b7fee0322c8efe5bb3713aa0b622488b
FEBRUARYTAR <FEBRUARYTAR@GMAIL.com>
2019-04-02 09:51:52 +0700

    configure the signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  get 'signup', to:'user#new', as: 'signup'
+  get 'signup', to:'users#new', as: 'signup'
   get 'login', to:'sessions#new', as: 'login'
   get 'logout', to:'sessions#destroy', as: 'logout'
 
